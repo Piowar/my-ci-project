d79e18ba41101c7d04aa6df9961d4e4a54503ace
Antoni <anton.wich2003@gmail.com>
2025-12-21 18:16:35 +0100

    Initial commit with basic CI setup

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        // Ten test zawsze przejdzie, co pozwoli nam zobaczyć "zielony" build w CI
+        assertTrue(true);
+    }
+}
