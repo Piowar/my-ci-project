6cb480af782b51ca01ba8f55be3ffd7f3f8f91f5
Piowar <anton.wich2003@gmail.com>
2026-01-13 14:02:54 +0100

    Update Bad.java

--- src/test/java/Bad.java
+++ src/test/java/Bad.java
@@ -1,14 +1,31 @@
-public class Bad {
+public class Bad { 
 
-    public static int divide(int a, int b) {
-        return a / b; // !! bug: potencjalne dzielenie przez 0 !!
-    }
-
-    public static String getPassword() {
-        return "admin123"; // !! code smell: hardcoded secret !!
-    }
+    // 1. SECURITY HOTSPOT: Hasło wpisane na sztywno
+    // SonarQube wykryje to jako zagrożenie bezpieczeństwa (Security Rating E)
+    private static final String HARDCODED_PASSWORD = "mySuperSecretPassword123";
 
     public static void main(String[] args) {
-        System.out.println(divide(10, 0)); // !! 100% crash !!
+        // 2. CODE SMELL: Użycie System.out zamiast Loggera
+        System.out.println("Start aplikacji...");
+
+        int a = 10;
+        int b = 0;
+
+        // 3. BUG: Dzielenie przez zero
+        // SonarQube wykryje to jako błąd krytyczny (Reliability Rating D/E)
+        if (true) {
+            int result = a / b;
+            System.out.println("Wynik: " + result);
+        }
+
+        // 4. CODE SMELL: Nieużywana zmienna
+        int unusedVariable = 123;
+        
+        // 5. CODE SMELL: Zakomentowany kod
+        // int x = 1;
+    }
+    
+    // 6. CODE SMELL: Pusta metoda
+    public void emptyMethod() {
     }
 }
