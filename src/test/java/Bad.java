a19bb63f5cdc7020aa571f4163573581c0abfcfe
Piowar <anton.wich2003@gmail.com>
2026-01-13 14:13:26 +0100

    Update Bad.java

--- src/test/java/Bad.java
+++ src/test/java/Bad.java
@@ -1,6 +1,20 @@
-public class Bad {
+public class Bad { // <--- Ta nazwa jest kluczowa! Musi pasować do nazwy pliku.
+
     public static void main(String[] args) {
-        // FIXME: To jest celowy błąd, który SonarQube MUSI zobaczyć!
-        System.out.println("Test");
+        System.out.println("Start aplikacji...");
+
+        int number = 10;
+        int zero = 0;
+
+        // BŁĄD KRYTYCZNY (Reliability: C)
+        // SonarQube wykryje: "Fix this call that leads to a division by zero"
+        int result = number / zero; 
+
+        System.out.println("Wynik: " + result);
+    }
+
+    // Code Smell (Maintainability: A - lekkie uwagi)
+    // Pusta metoda, której nikt nie używa
+    public void emptyMethod() {
     }
 }
