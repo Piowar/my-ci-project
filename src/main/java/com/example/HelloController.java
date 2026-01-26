cb5a78264dc7092b5a2aadfacdbf1feac09e62c4
Piowar <anton.wich2003@gmail.com>
2026-01-26 14:26:38 +0100

    Create HelloController.java

---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,11 @@
+package com.example;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/api/hello")
+    public String sayHello() {
+        return "Aplikacja działa i została wdrożona przez GitHub Actions!";
+    }
+}
